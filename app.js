8dbaf50cb109398a617b0149e2636c16019dfc5f
fredcardoso191 <fredcardoso01@hotmail.com>
2022-09-28 16:39:25 -0300

    added new routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import router from "./routes.js";
+import VideoController from './video/VideoController.js'
 
 const app = express();
 const port = 8282;
@@ -10,7 +11,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('frontend'));
 app.set('view engine', 'ejs');
 
-app.use('/', router);
+app
+    .use('/', router)
+    .use('/', VideoController);
 
 app.listen(port, () => {
     console.log('Servidor aberto em http://localhost:' + port);
